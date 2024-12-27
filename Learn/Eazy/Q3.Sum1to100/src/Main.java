a69fdc9f620127608241f9a7205198eecb4cd5d0
Zerom34 <notod34@gmail.com>
2024-12-27 03:07:36 +0300

    Q 3-5 Done

--- Learn/Eazy/Q3.Sum1to100/src/Main.java
+++ Learn/Eazy/Q3.Sum1to100/src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum = sum + i;
+          System.out.println("Сумма от 1 до "+ i + " равна "  +sum);
+        }
+        System.out.println("END Var 1");
+        System.out.println("");
+        sum = 0;
+        for (int i = 0; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println("Сумма от 1 до 100" + " равна "  +sum);
     }
 }
