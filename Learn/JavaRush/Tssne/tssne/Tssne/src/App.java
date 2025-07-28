1a058cabe7e4389e9728656d99ab00dcbe241070
Zerom34 <notod34@gmail.com>
2025-07-28 10:59:48 +0300

    28_07_25_V1

---
+++ Learn/JavaRush/Tssne/tssne/Tssne/src/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+/* 
+Второе минимальное число из введенных
+*/
+
+public class App {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+
+        int min = Integer.MAX_VALUE;
+        int secondMin = Integer.MAX_VALUE;
+
+        while (console.hasNextInt()) {
+            int tmp = console.nextInt();
+            if (tmp < min) {
+                secondMin = min;
+                min = tmp;
+            } else if (tmp > min && tmp < secondMin) {
+                secondMin = tmp;
+            }
+        }
+
+        System.out.println(secondMin);
+    }
+}
